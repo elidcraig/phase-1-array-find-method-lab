160b5dd0192cf5a32a0a132d065d406d6a39eb82
Eli Craig <elidcraig@gmail.com>
2022-04-27 10:06:53 -0700

    commit tree 473a053e6b835763cb60305ccc6cff54d1145141

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = record.find(isWin)
+    if (typeof result === 'object') {
+        return result.year
+    } else {
+        return result
+    }
+}
+
+function isWin(season) {
+    return (season.result === 'W')
+}
